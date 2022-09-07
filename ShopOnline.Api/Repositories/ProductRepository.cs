40394e79a558f6579ae51db77ec6f6921a17514f
Andres Velasquez <andresvel00@gmail.com>
2022-09-06 21:05:20 -0500

    API for products

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await this.shopOnlineDbContext.Products.ToListAsync();
+            return products;
+        }
+
+        public Task<Product> GetProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductCategory> GetProductCategory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
